d9b90a11be3daeff129ea146300fc77282e05831
Violet <violet@def.au>
2024-11-16 18:36:03 +1100

    1.4

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
-Search linkwarden from alfred.
+A linkwarden client for alfred.
 
 [Download Here](https://github.com/deafmute1/alfred-linkwarden/releases/latest/download/Linkwarden.Search.alfredworkflow)
 
-## Usage
-You can search all links with the default keyword, `lw {query}`
+## Setup 
+1. Set linkwarden API key 
+2. Set instance URL
 
-You can search within a collection/show all items in collection ordered by date added by adding a custom keyword. An example is given in the workflow object. You need to create a `Script Filter` object, and add `bash ./helper.sh collection 16 "$@"` as the script value, where `16` is the specific collection id. You can retrieve this id by looking at the url of a collection when it is open in your browse, it will be the final value in the path.
+## Usage 
+To display/search all links: `lw {query}`
+- To open in browser, return the link.
+- To delete a link, use `Shift` modifier
+- To open a preserved version on your linkwarden server use `cmd/⌘`.
 
-You can delete links using the cmd/⌘ modifier.
+To display/search all collections: `lwc {query}`
+- You can display/search each collection by returning it. The contents of each collection are searchable and have the same modifier options as `lw`
+- You can open the collection in your browser with `cmd/⌘` modifier
 
-TODO
-- Add options to action url in other browser, incognito etc.
-- More options to browse linkwarden (e.g. collections)
-- Download and display favicons
+You can search/display a collection via a custom keyword. 
+- An example is given in the workflow object. 
+- You need create a `Script Filter` with the script `bash ./helper.sh collection 16 "$@"`, where `16` is the specific collection id.
+-  You can retrieve this id by looking at the url of a collection when it is open in your browse, it will be the final value in the path.
+
+
+## TODO
+- Download and display favicons? - expensive
+- Show generic icons for collections, links and each preserved format
--- workflow/alfred-linkwarden.py
+++ workflow/alfred-linkwarden.py
@@ -2,10 +2,19 @@ import shutil
 import subprocess
 import os 
 import sys 
-import urllib.parse
 
 import requests 
 from workflow import Workflow 
+
+SAVED_FORMATS = {
+    "Screenshot": "1", 
+    "PDF": "2",
+    "Readable": "3",
+    "Webpage Copy": "4"
+}
+
+def lw_url() -> str:
+    return os.environ['LW_URL'].rstrip('/')
 
 def get_links(query: str|None, collection_id: str|None = None): 
     params = {
@@ -16,35 +25,24 @@ def get_links(query: str|None, collection_id: str|None = None):
     if query is not None: params['searchQueryString'] = query 
     if collection_id is not None: params['collectionId'] = collection_id
     return requests.get(
-        url=urllib.parse.urljoin(os.environ['LW_URL'], "/api/v1/links"),
-        headers={"Authorization": f"Bearer {os.environ["LW_API_KEY"]}"}, 
+        url=f"{lw_url()}/api/v1/links",
+        headers={"Authorization": f"Bearer {os.environ['LW_API_KEY']}"}, 
         params=params       
     )
     
 def delete_link(link_id: str): 
     return requests.delete(
-        url=urllib.parse.urljoin(os.environ['LW_URL'], f"/api/v1/links/{link_id}"), 
+        url=f"{lw_url()}/api/v1/links/{link_id}", 
+        headers={"Authorization": f"Bearer {os.environ['LW_API_KEY']}"},
+    )
+
+def get_all_collections(): 
+    return requests.get(
+        url=f"{lw_url()}/api/v1/collections",
         headers={"Authorization": f"Bearer {os.environ["LW_API_KEY"]}"},
     )
     
-def proc_args_lw(args: list) -> requests.Response:
-    if args[0] == "link": 
-        return get_links(' '.join(args[1:]), None)
-    elif args[0] == "collection": 
-        if len(args) > 2:
-            query = ' '.join(args[2:]) 
-        else:
-            query = None
-        return get_links(query, args[1])
-    elif args[0] == "delete": 
-        delete_link(args[1])
-        sys.exit()
-    else: 
-        sys.exit()
-        
-def main(workflow):
-    response = proc_args_lw(sys.argv[1:])
-    
+def query_json_to_workflow_item(response: requests.Response):
     for link in response.json()["response"]:
         item = workflow.add_item(
             title=link["name"],
@@ -56,8 +54,68 @@ def main(workflow):
         mod = item.add_modifier(
             "cmd", subtitle="Delete Entry", arg=str(link["id"])
         )
-
     workflow.send_feedback()
+    
+def links_to_workflow_items(workflow: Workflow, response: requests.Response):
+    for link in response.json()["response"]:
+        item = workflow.add_item(
+            title=link["name"],
+            subtitle=link["url"],
+            copytext=link["url"],
+            arg=link["url"],
+            valid=True
+        )
+        mod = item.add_modifier(
+            "shift", subtitle="Delete Entry", arg=str(link["id"])
+        )
+        mod = item.add_modifier(
+            "cmd", subtitle="Open preserved version", arg=str(link["id"])
+        )
+    workflow.send_feedback()
+
+def collections_to_workflow_items(workflow: Workflow, response: requests.Response): 
+    for c in response.json()["response"]:
+        item = workflow.add_item( 
+            title=c["name"],
+            subtitle=c["description"],
+            copytext=f"{lw_url()}/collections/{c['id']}",
+            arg=str(c['id']),
+            valid=True
+        )
+        mod = item.add_modifier(
+            "cmd", subtitle="Open in Browser"
+        )
+    workflow.send_feedback()
+    
+def main(workflow: Workflow) -> requests.Response:
+    args = sys.argv[1:]
+    if args[0] == "link":
+        # alfred-linkwarden.py link <QUERY (STR)> 
+        links_to_workflow_items(workflow, get_links(' '.join(args[1:]), None))
+    elif args[0] == "collection":
+        # alfred-linkwarden.py collection <COLLECTION ID (INT)>  <QUERY>
+        if len(args) > 2:
+            query = ' '.join(args[2:]) 
+        else:
+            query = None
+        links_to_workflow_items(workflow, get_links(query, args[1]))
+    elif args[0] == "collections": 
+        # alfred-linkwarden.py collections
+        collections_to_workflow_items(workflow, get_all_collections())
+    elif args[0] == "delete": 
+        # alfred-linkwarden.py delete <LINK ID (INT)>
+        delete_link(args[1])
+    elif args[0] == "saved": 
+        base=f"{lw_url()}/preserved/{args[1]}?format="
+        for k, v in SAVED_FORMATS.items(): 
+            workflow.add_item(
+                title=f"Open {k}",
+                copytext=base + v, 
+                arg=base + v, 
+                valid=True
+            )
+        workflow.send_feedback()
+
     
 if __name__ == "__main__":
     # if somehow not inside venv, force recreate venv and rerun helper script
